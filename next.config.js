ef7a5bd44fb18d76714b06ec13c0f575aa5c4cc2
yrizhniv <yrizhniv@griddynamics.com>
2021-06-06 16:20:55 +0300

    feat: add notification api & custom service-worker

--- next.config.js
+++ next.config.js
@@ -3,9 +3,11 @@ const path = require('path');
 const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
+  future: { webpack5: true },
   pwa: {
     dest: 'public',
-    runtimeCaching
+    runtimeCaching,
+    swSrc: 'service-worker.js'
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')]
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,81 @@
-import styles from '../styles/Home.module.css'
+import { useState, useEffect } from 'react';
+
+import styles from '../styles/Home.module.css';
+
+const base64ToUint8Array = (base64) => {
+  const padding = '='.repeat((4 - (base64.length % 4)) % 4);
+  const b64 = (base64 + padding).replace(/-/g, '+').replace(/_/g, '/');
+
+  const rawData = window.atob(b64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+};
 
 export default function Home() {
+  const [isSubscribed, setIsSubscribed] = useState(false);
+  const [subscription, setSubscription] = useState(null);
+  const [registration, setRegistration] = useState(null);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && 'serviceWorker' in navigator && window.workbox !== undefined) {
+      // run only in browser
+      navigator.serviceWorker.ready.then((reg) => {
+        reg.pushManager.getSubscription().then((sub) => {
+          if (sub && !(sub.expirationTime && Date.now() > sub.expirationTime - 5 * 60 * 1000)) {
+            setSubscription(sub);
+            setIsSubscribed(true);
+          }
+        });
+        setRegistration(reg);
+      });
+    }
+  }, []);
+
+  const subscribeButtonOnClick = async (event) => {
+    event.preventDefault();
+    const sub = await registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: base64ToUint8Array(process.env.NEXT_PUBLIC_WEB_PUSH_PUBLIC_KEY)
+    });
+    // TODO: you should call your API to save subscription data on server in order to send web push notification from server
+    setSubscription(sub);
+    setIsSubscribed(true);
+    console.log('web push subscribed!');
+    console.log(sub);
+  };
+
+  const unsubscribeButtonOnClick = async (event) => {
+    event.preventDefault();
+    await subscription.unsubscribe();
+    // TODO: you should call your API to delete or invalidate subscription data on server
+    setSubscription(null);
+    setIsSubscribed(false);
+    console.log('web push unsubscribed!');
+  };
+
+  const sendNotificationButtonOnClick = async (event) => {
+    event.preventDefault();
+    if (subscription == null) {
+      console.error('web push not subscribed');
+      return;
+    }
+
+    await fetch('/api/notification', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        subscription,
+        message: 'hello from client!'
+      })
+    });
+  };
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
@@ -9,9 +84,20 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Get started by editing <code className={styles.code}>pages/index.js</code>
         </p>
+
+        <div>
+          <button onClick={subscribeButtonOnClick} disabled={isSubscribed}>
+            Subscribe
+          </button>
+          <button onClick={unsubscribeButtonOnClick} disabled={!isSubscribed}>
+            Unsubscribe
+          </button>
+          <button onClick={sendNotificationButtonOnClick} disabled={!isSubscribed}>
+            Send Notification
+          </button>
+        </div>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
@@ -24,10 +110,7 @@ export default function Home() {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
+          <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
@@ -37,9 +120,7 @@ export default function Home() {
             className={styles.card}
           >
             <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
           </a>
         </div>
       </main>
@@ -50,10 +131,9 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
